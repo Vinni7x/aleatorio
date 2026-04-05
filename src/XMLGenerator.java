46763f81858cbba5f12a7ef9c9e38fdda646267f
Marcos Vinicius <marcos.jansem@discente.ufma.br>
2026-04-05 18:45:29 -0300

    feat: adiciona XMLGenerator para gravar saída T.xml

--- src/XMLGenerator.java
+++ src/XMLGenerator.java
@@ -1,4 +1,24 @@
+import java.io.*;
+import java.util.List;
 
 public class XMLGenerator {
 
+
+    public static void write(List<Token> tokens, String outputPath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(outputPath), "UTF-8"))) {
+
+            writer.write("<tokens>");
+            writer.newLine();
+
+            for (Token token : tokens) {
+                if (token.tag == TokenType.EOF) continue;
+                writer.write(token.toXML());
+                writer.newLine();
+            }
+
+            writer.write("</tokens>");
+            writer.newLine();
+        }
+    }
 }
