14026c78dd1402e724bbeb49b5d9cf4a0c232ec9
Marcos Vinicius <marcos.jansem@discente.ufma.br>
2026-04-05 15:31:56 -0300

    feat: adiciona método toXML e escape de caracteres no Token

--- src/Token.java
+++ src/Token.java
@@ -1,7 +1,7 @@
 
 public class Token {
 	 public final TokenType tag; 
-	    public final String value;  
+	 public final String value;  
 
 	    public Token(TokenType tag, String value) {
 	        this.tag   = tag;
@@ -12,4 +12,21 @@ public class Token {
 	    	return tag; }
 	    public String    getValue() { 
 	    	return value; }
-} 
+
+public String toXML() {
+    return "<" + tag.getXmlTag() + "> " + escapeXml(value) + " </" + tag.getXmlTag() + ">";
+}
+
+private static String escapeXml(String s) {
+    
+    return s.replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;"); 
+}
+ 
+@Override
+public String toString() {
+    return "<" + tag + ", " + value + ">";  
+}
+}
